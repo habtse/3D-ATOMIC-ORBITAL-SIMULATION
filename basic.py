06854b221823a5fd4b6cd3915d3c43101b06963d
habtamu <habtetsegaye19@gmail..com>
2022-06-16 23:24:31 +0300

    s_orbital.obj   and s_loader.py file are added and some update on basic.py

--- basic.py
+++ basic.py
@@ -2,7 +2,17 @@ import pygame as pg
 from OpenGL.GL import *
 from OpenGL.GL.shaders import compileProgram,compileShader
 import numpy as np
+from s_loader import Mesh
+import pyrr
 
+
+class Cube:
+
+
+    def __init__(self, position, eulers):
+
+        self.position = np.array(position, dtype=np.float32)
+        self.eulers = np.array(eulers, dtype=np.float32)
 class App:
     def __init__(self):
         #initialise pygame
@@ -11,11 +21,34 @@ class App:
         pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
         pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK,
                                     pg.GL_CONTEXT_PROFILE_CORE)
-        pg.display.set_mode((640,480), pg.OPENGL|pg.DOUBLEBUF)
+        pg.display.set_mode((1200,900), pg.OPENGL|pg.DOUBLEBUF)
         self.clock = pg.time.Clock()
         #initialise opengl
         glClearColor(0.1, 0.2, 0.2, 1)
+        
        
+        # mesh
+        self.mesh = Mesh('model/s_orbital.obj')
+        print(self.mesh.vertex_count)
+         # shader 
+        self.program = self.createShader("shaders/vertex.txt", "shaders/fragment.txt")
+        
+       
+        # projection transform
+        projection = pyrr.matrix44.create_perspective_projection(fovy = 45, aspect = 640/480, 
+            near = 0.1, far =50000, dtype=np.float32)
+        translate = pyrr.matrix44.create_from_translation(np.array([0, 0, -200], np.float32))
+        identity = pyrr.matrix44.create_identity(dtype=np.float32)
+        rotated = pyrr.matrix44.multiply(
+            identity
+        
+            , pyrr.matrix44.create_from_x_rotation(theta=np.radians(45),dtype=np.float32,))
+        translate = pyrr.matrix44.multiply(
+            rotated, translate
+        )
+        glUseProgram(self.program)
+        glUniformMatrix4fv(glGetUniformLocation(self.program,'projection'),1,GL_FALSE,projection)
+        glUniformMatrix4fv(glGetUniformLocation(self.program,'model'),1,GL_FALSE,translate)
         self.mainLoop()
     
     def createShader(self, vertexFilepath, fragmentFilepath):
@@ -40,13 +73,17 @@ class App:
                     running = False
             #refresh screen
             glClear(GL_COLOR_BUFFER_BIT)
-            glPointSize(10)
-            glLineWidth(5)
+            # glPointSize(10)
+            # glLineWidth(5)
 
+            glUseProgram(self.program)
+            glBindVertexArray(self.mesh.vao)
+
+            glDrawArrays(GL_TRIANGLES, 0, self.mesh.vertex_count)
             pg.display.flip()
 
             #timing
             self.clock.tick(60)
         self.quit()
 
-   
+App()
